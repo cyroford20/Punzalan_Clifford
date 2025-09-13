1a48febb630dc142081876d91b7fea9c32ff2080
Clifford <punzalan.clifford@minsu.edu.ph>
2025-09-13 15:56:56 +0800

    Update configuration for production deployment

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://crud-lavalust123.onrender.com';
+$config['base_url'] 				= 'https://crud-lavalust5.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
