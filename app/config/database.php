dc9ef9f6454a4b6eb7d21e0b03f07decad291180
Clifford <punzalan.clifford@minsu.edu.ph>
2025-10-08 17:56:00 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -59,15 +59,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: 'sql12.freesqldatabase.com',
-    'port'		=> getenv('DB_PORT') ?: '3306',
-    'username'	=> getenv('DB_USER') ?: 'sql12799948',
-    'password'	=> getenv('DB_PASS') ?: '7rVZplLkP5',
-    'database'	=> getenv('DB_NAME') ?: 'sql12799948',
+    'hostname'	=> getenv('DB_HOST'),
+    'port'		=> getenv('DB_PORT'),
+    'username'	=> getenv('DB_USER'),
+    'password'	=> getenv('DB_PASS'),
+    'database'	=> getenv('DB_NAME'),
     'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-    // Optional for SQLite
-    'path'      => ''
+    'dbprefix'	=> '',    
 );
 
 ?>
